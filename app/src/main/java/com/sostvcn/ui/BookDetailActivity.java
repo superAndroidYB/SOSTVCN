c948746f7bc87d45bdfdbc7237e702cd0a1f5574
yubin-com <646683259@qq.com>
2017-08-23 09:53:54 +0800

    添加书籍详情页面

---
+++ app/src/main/java/com/sostvcn/ui/BookDetailActivity.java
@@ -0,0 +1,22 @@
+package com.sostvcn.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sostvcn.R;
+
+public class BookDetailActivity extends BaseActivity {
+
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_book_detail;
+    }
+
+    @Override
+    protected void onInitView(Bundle bundle) {
+
+    }
+
+
+}
